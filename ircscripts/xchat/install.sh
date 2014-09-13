7cc8581472a48350fc4a779e43d6105bdaa26926
Valentin Ilie <valentin.ilie@gmail.com>
2014-09-13 17:10:23 +0300

    ircscripts: Add xchat scripts
    Signed-off-by: Valentin Ilie <valentin.ilie@gmail.com>

---
+++ ircscripts/xchat/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Please enter your freenode password"
+read text
+
+# TODO Replace dummypassword from serverlist with read password before install
+# TODO Replace relative paths in config files with the local paths
+
+echo "Replace password not yet implemented"
+cp -r . ~/.xchat2/

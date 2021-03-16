14c359279a5101e519589a962173aa32d47ed9a9
Valentin Ilie <valentin.ilie@gmail.com>
2021-03-16 14:14:38 +0000

    rpi: brute: change interface to wlan
    Signed-off-by: Valentin Ilie <valentin.ilie@gmail.com>

--- rpi/brute.sh
+++ rpi/brute.sh
@@ -4,5 +4,5 @@ iptables -N SSHATTACK
 iptables -A SSHATTACK -j LOG --log-prefix "SSH bruteforce!" --log-level 7
 iptables -A SSHATTACK -j DROP
 
-iptables -A INPUT -i eth0 -p tcp -m state --dport 22 --state NEW -m recent --set
-iptables -A INPUT -i eth0 -p tcp -m state --dport 22 --state NEW -m recent --update --seconds 120 --hitcount 4 -j SSHATTACK
+iptables -A INPUT -i wlan0 -p tcp -m state --dport 22 --state NEW -m recent --set
+iptables -A INPUT -i wlan0 -p tcp -m state --dport 22 --state NEW -m recent --update --seconds 120 --hitcount 4 -j SSHATTACK
